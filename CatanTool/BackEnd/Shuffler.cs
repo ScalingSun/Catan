48e0adffa7c823019df10b8aeaa1ba4c09e078d9
Wang,Mike M <mike.wang@student.fontys.nl>
2019-10-03 15:11:33 +0200

    x d

--- CatanTool/BackEnd/Shuffler.cs
+++ CatanTool/BackEnd/Shuffler.cs
@@ -6,7 +6,7 @@ namespace BackEnd
 {
     public class Shuffler
     {
-        TimeoutException blijf gat 
+        TimeoutException blijf ga
         //totally didnt copy paste this from the web 
         private Random rng = new Random();
 
