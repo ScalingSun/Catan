8fdce7dd62952b5d9fc1dbcf3a0d4ea6c815dd15
Wang,Mike M <mike.wang@student.fontys.nl>
2019-10-03 15:05:08 +0200

    hhhh

--- CatanTool/BackEnd/Shuffler.cs
+++ CatanTool/BackEnd/Shuffler.cs
@@ -6,7 +6,7 @@ namespace BackEnd
 {
     public class Shuffler
     {
-        umomgae
+        umomgaesdsss
         //totally didnt copy paste this from the web 
         private Random rng = new Random();
 
