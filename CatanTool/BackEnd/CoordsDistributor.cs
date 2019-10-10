3d086f217ce62e2b2ce41c27c9e2cf7014d310d1
Wang,Mike M <mike.wang@student.fontys.nl>
2019-10-10 11:06:28 +0200

    - added coord grid - removed unintented code? for shuffler.cs - changed type enum harbour to harbourOneToThree

--- CatanTool/BackEnd/CoordsDistributor.cs
+++ CatanTool/BackEnd/CoordsDistributor.cs
@@ -14,8 +14,16 @@ namespace BackEnd
         {
             Type = type;
             coords = new List<Coordinate>();
-            //FIX DIT
-            coords.Add(new Coordinate(1,0));coords.Add(new Coordinate(2,0)); coords.Add(new Coordinate(3,0)); coords.Add(new Coordinate(4, 0));
+            coords.AddRange(new List<Coordinate>
+            {
+                new Coordinate(1,1), new Coordinate(1,2), new Coordinate(1,3),new Coordinate(1,4),
+                new Coordinate(2,1), new Coordinate(2,2), new Coordinate(2,3),new Coordinate(2,4), new Coordinate(2,5),
+                new Coordinate(3,1), new Coordinate(3,2), new Coordinate(3,3),new Coordinate(3,4), new Coordinate(3,5), new Coordinate(3,6),
+                new Coordinate(4,1), new Coordinate(4,2), new Coordinate(4,3),new Coordinate(4,4), new Coordinate(4,5), new Coordinate(4,6), new Coordinate(4,7),
+                new Coordinate(5,1), new Coordinate(5,2), new Coordinate(5,3),new Coordinate(5,4), new Coordinate(5,5), new Coordinate(5,6),
+                new Coordinate(6,1), new Coordinate(6,2), new Coordinate(6,3),new Coordinate(6,4), new Coordinate(6,5),
+                new Coordinate(7,1), new Coordinate(7,2), new Coordinate(7,3),new Coordinate(7,4),
+            });
         }
         public int GetOneRandomCoordinate()
         {
