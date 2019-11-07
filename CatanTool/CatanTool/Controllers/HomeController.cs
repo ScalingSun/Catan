88e2d93203608f2a5f9fd0be3318767e5c277626
ScalingSun <timwilms@live.nl>
2019-11-07 14:36:00 +0100

    Canvas test (#6)
    * Kleine test voor tekenen van grid
    
    * Methodes voor tekenen verplaatst naar Visualiser en comments toegevoegd.
    
    * Playboard van vorige commit is hernoemd naar Visualiser.
    
    * Playboard hernoemd naar Visualiser als model op de index pagina.
    
    * Cijfers van tegels worden op de tegel getekend die erbij hoort.
    
    * Visualiser verplaatst naar backend en aangepast met de huidige klassen.
    
    * Simpele weergave voor havens zonder verbinding met junctions.
    
    * Kleuren aangepast

--- CatanTool/CatanTool/Controllers/HomeController.cs
+++ CatanTool/CatanTool/Controllers/HomeController.cs
@@ -5,14 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CatanTool.Models;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using BackEnd;
 
 namespace CatanTool.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
-            return View();
+            Visualiser visualiser = new Visualiser();
+            List<ITile> tiles = new List<ITile>()
+            {
+                new WaterTile(0,0,EnumTileType.Sea), new HarbourTile(1,0,EnumTileType.TwoMeadowHarbour), new WaterTile(2,0,EnumTileType.Sea), new WaterTile(3,0,EnumTileType.Sea),
+                new WaterTile(0,1,EnumTileType.Sea), new LandTile(1,1,EnumTileType.Wood, 2), new LandTile(2,1,EnumTileType.Wood, 2), new LandTile(3,1,EnumTileType.Meadow, 8), new WaterTile(4,1,EnumTileType.Sea)
+            };
+
+            Playboard pb = new Playboard(visualiser.DrawMap(tiles));
+            
+            return View(pb);
         }
 
         public IActionResult About()
