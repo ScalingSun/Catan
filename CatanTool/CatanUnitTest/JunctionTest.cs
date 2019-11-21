b2d177447fabf99f4f8fb0868857c1b3c66aba05
Nick <nick.hagens@hotmail.com>
2019-11-21 11:21:45 +0100

    Extra tests gemaakt om te helpen met debuggen

--- CatanTool/CatanUnitTest/JunctionTest.cs
+++ CatanTool/CatanUnitTest/JunctionTest.cs
@@ -77,6 +77,36 @@ namespace CatanUnitTest
             // Assert
             Assert.Equal(expectedCount, actual.Count);
         }
+        
+        [Fact]
+        public void FindAllAdjecentTiles()
+        {
+            // Arrange
+            int expectedFoundCount = 6;
+
+            List<ITile> tiles = new List<ITile>
+            {
+                new LandTile(new Coordinate(0,0,EnumCoordinateType.Land), EnumLandTileType.Desert, 1),
+                new LandTile(new Coordinate(0,1,EnumCoordinateType.Land), EnumLandTileType.Desert, 2),
+                new LandTile(new Coordinate(1,0,EnumCoordinateType.Land), EnumLandTileType.Desert, 3),
+                new LandTile(new Coordinate(1,1,EnumCoordinateType.Land), EnumLandTileType.Desert, 4),
+                new LandTile(new Coordinate(1,2,EnumCoordinateType.Land), EnumLandTileType.Desert, 5),
+                new LandTile(new Coordinate(2,1,EnumCoordinateType.Land), EnumLandTileType.Desert, 6),
+                new LandTile(new Coordinate(2,2,EnumCoordinateType.Land), EnumLandTileType.Desert, 7)
+            };
+
+            Map map = new Map(tiles);
+
+            // Act
+            List<ITile> foundTiles = map.GetAdjacentTiles(new Coordinate(1, 1, EnumCoordinateType.Land));
+            int actualFoundCount = foundTiles.Count();
+
+            int originTileCount = foundTiles.Where(t => t.Coordinate.Xaxis == 1 && t.Coordinate.Yaxis == 1).Count();
+
+            // Assert
+            Assert.Equal(0, originTileCount);
+            Assert.Equal(expectedFoundCount, actualFoundCount);
+        }
 
         [Fact]
         public void NoSameTilesInJunction()
